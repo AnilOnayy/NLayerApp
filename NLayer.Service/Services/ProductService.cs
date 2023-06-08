abbccfd7e934613c94c511a14c092c7557c18222
Anıl Onay <cengizhananilonay@gmail.com>
2023-06-08 16:59:51 +0300

    ProductsWithCategory Endpoint Added

---
+++ NLayer.Service/Services/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs.ProductDTOs;
+using NLayer.Core.DTOs.ResponseDTOs;
+using NLayer.Core.Entities;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IProductRepository productRepository,IMapper mapper) : base(repository, unitOfWork)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetWithCategoryAsync()
+        {
+            var products = await  _productRepository.GetProductsWithCategoryAsync();
+            var productDtos = _mapper.Map<List<ProductWithCategoryDto>>(products);
+
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productDtos);
+
+        }
+    }
+}
