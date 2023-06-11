11629fe6f9a931f4f7aec082c6610d2f7c80a9b2
Anıl Onay <cengizhananilonay@gmail.com>
2023-06-11 21:50:22 +0300

    Caching on memory Added

---
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.Extensions.Caching.Memory;
+using NLayer.Core.DTOs.ProductDTOs;
+using NLayer.Core.DTOs.ResponseDTOs;
+using NLayer.Core.Entities;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Service.Exceptions;
+using System.Linq.Expressions;
+
+namespace NLayer.Caching
+{
+    public class ProductServiceWithCaching : IProductService
+    {
+
+        private const string CacheProductKey = "productsCache";
+
+        private readonly IMapper _mapper;
+        private readonly IMemoryCache _memoryCache;
+        private readonly IProductRepository _repo;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductServiceWithCaching(IMapper mapper, IMemoryCache memoryCache, IProductRepository repo, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _memoryCache = memoryCache;
+            _repo = repo;
+            _unitOfWork = unitOfWork;
+
+            if (! _memoryCache.TryGetValue(CacheProductKey, out _))
+            {
+                memoryCache.Set(CacheProductKey, _repo.GetProductsWithCategoryAsync().Result);
+            }
+        }
+
+        public async Task<Product> AddAsync(Product entity)
+        {
+            await _repo.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProducts();
+
+            return entity;
+
+        }
+
+        public async  Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            await _repo.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProducts();
+
+            return entities;
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<Product, bool>> expression)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            var products = _memoryCache.Get<IEnumerable<Product>>(CacheProductKey);
+
+            if(products==null)
+            {
+                throw new NotFoundException("Products not found");
+            }
+
+            return Task.FromResult(products);
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            var product = _memoryCache.Get<List<Product>>(CacheProductKey).FirstOrDefault(x => x.Id == id);
+
+            if(product==null)
+            {
+                throw new NotFoundException("Product not found");
+            }
+
+            return Task.FromResult(product);
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetWithCategoryAsync()
+        {
+            var products =  _memoryCache.Get<List<Product>>(CacheProductKey).ToList();
+            var data = _mapper.Map<List<ProductWithCategoryDto>>(products);
+
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, data);
+        }
+
+        public async Task RemoveAsync(Product entity)
+        {
+            _repo.Remove(entity);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProducts();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<Product> entities)
+        {
+            _repo.RemoveRange(entities);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProducts();
+        }
+
+        public async Task UpdateAsync(Product entity)
+        {
+            _repo.Update(entity);
+            await _unitOfWork.CommitAsync();
+            await CacheAllProducts();
+        }
+
+        public IQueryable<Product> Where(Expression<Func<Product, bool>> expression)
+        {
+            return _memoryCache.Get<List<Product>>(CacheProductKey).Where(expression.Compile()).AsQueryable();
+        }
+
+        public async Task CacheAllProducts()
+        {
+
+            _memoryCache.Set(CacheProductKey, await _repo.GetAll().ToListAsync());
+        }
+    }
+}
